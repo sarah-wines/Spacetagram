95c62d05adb12414f4f9efd08b77cfaddcaeef26
sarah-wines <sarah.lodico.wines@gmail.com>
2022-01-18 15:27:11 -0500

    adds open loading animation

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,20 +2,25 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Image } from './Image.js';
 import { Container } from 'react-bootstrap';
+import { Loading } from './Loading';
 export function ImageList() {
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     axios
       .get(
         `https://api.nasa.gov/planetary/apod?api_key=8cGPvrIOA5TQd7CdnZyeQTdEWQvfbftSlpyaDzkp&count=10`
       )
       .then((response) => {
-        // console.log(response.data);
-
         setImages(response.data);
+        setIsLoading(false);
       });
   }, []);
-  return (
+  return isLoading ? (
+    <>
+      <Loading />
+    </>
+  ) : (
     <>
       <Container fluid="sm">
         <div className="titles">
